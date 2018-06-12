4fd14078414cf5bbb12da659b81f010e1a1c76d7
Knox <Sydney.Knox@parivedasolutions.com>
2018-06-12 16:48:06 -0500

    badges formatted

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ var optInLink = document.getElementById('optInLink');
 optInLink.addEventListener("click", function(){
     myDiv.innerText = myDiv.innerText + 1;
 });
+
+
 //myDiv.innerText = "Hello from Sydney";
